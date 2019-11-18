2d011e2d5aa9bb8067c39e58995bc07ed5066dbb
ajisrael <alexisraels@Alexs-MBP-2.lan1>
2019-11-17 23:41:19 -0500

    changed kenc to res

--- School/Security/Programs/prog4/fcrypt.c
+++ School/Security/Programs/prog4/fcrypt.c
@@ -310,18 +310,19 @@ int main (int argc, char* argv[])
         EVP_CIPHER_CTX_set_key_length(ctx, fstats.st_size);
         EVP_DecryptInit_ex(ctx, NULL, NULL, kPass, ivec);
         messLen = 0;
+        res = (unsigned char *) malloc(ctLen);
 
         /// Print out Kenc in hexadecimal
-        fprintf(stdout, "Pre-Decrypted Kenc: Length %d <", fstats.st_size);
-        printHex(stdout, kEnc, KEYLEN);
+        fprintf(stdout, "Pre-Decrypted Kenc: Length %d <", ctLen);
+        printHex(stdout, res, ctLen);
         fprintf(stdout, ">\n");
 
-        EVP_DecryptUpdate(ctx, &kEnc[outLen], &outLen, ciphertext, ctLen);
+        EVP_DecryptUpdate(ctx, res, &outLen, ciphertext, ctLen);
         messLen += outLen;
         printf("Kenc @ Outlen %d: <", outLen);
         printHex(stdout, &kEnc[outLen], KEYLEN - outLen);
         printf(">\n");
-        EVP_DecryptFinal_ex(ctx, &kEnc[outLen], &outLen);
+        EVP_DecryptFinal_ex(ctx, &res[outLen], &outLen);
         messLen += outLen;
 
         /// Tessting
@@ -330,7 +331,7 @@ int main (int argc, char* argv[])
 
         // Print out Kenc in hexadecimal
         fprintf(stdout, "Decrypted Kenc: <");
-        printHex(stdout, kEnc, KEYLEN);
+        printHex(stdout, res, KEYLEN);
         fprintf(stdout, ">\n");
 
         // Clean up memory
