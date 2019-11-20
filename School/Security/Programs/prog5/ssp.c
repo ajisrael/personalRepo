643d5b9ab6631a16e5df3dd4074e8a724ec1476a
ajisrael <ajisrael@mtu.edu>
2019-11-20 13:40:16 -0500

    Memory manager

--- School/Security/Programs/prog5/ssp.c
+++ School/Security/Programs/prog5/ssp.c
@@ -50,5 +50,132 @@
 //       8. Log content is not specified, explaination required in README.
 //       9. All other incorporated secure coding practices explained in README.
 // Assm: No file spooled by this application contains sensitive data.
+// Defn: MAXFILE = The maximum size of a spoolable file in bytes.
 //------------------------------------------------------------------------------
 
+#include <sys/types.h> // System data
+#include <sys/stat.h>  // System data
+#include <unistd.h>    // System data
+#include <stdlib.h>    // Memory management
+
+#define MAXFILE 250000000 // Maximum size of a spoolable file
+
+//------------------------------------------------------------------------------
+struct memManager
+{
+    char ptrs[256][2]; // Matrix of pointers to allocated memory & thier status
+    int  size;         // # of allocated pointers
+}
+
+struct memManager gMan; // Global memory manager
+gMan.size = 0;          // Size initalized to zero
+//------------------------------------------------------------------------------
+
+int allocMem(char * ptr, int size)
+//------------------------------------------------------------------------------
+// Name: allocMem
+// Func: Wrapper for global memory manager when mallocing memory.
+// Meth: Mallocs size bytes of memory, sets base addr of ptr, and updates the 
+//       global memory manager.
+// Args: ptr  = Base address of newly allocated memory.
+//       size = Size of newly allocated memory in bytes.
+// Retn:  0 = Everything is good.
+//       -1 = An error occured.
+//------------------------------------------------------------------------------
+{
+    ptr = malloc(size);
+    
+    if (ptr == NULL)
+    {
+        perror("alloc");
+        return -1;
+    }
+
+    gMan.ptrs[gMan.size][0] = &ptr; // Add ptr to memory manager
+    gMan.ptrs[gMan.size][1] = 1;    // Set status of ptr to alloced
+    gMan.size++;
+
+    return 0;
+}
+
+void freeMem(char * ptr)
+//------------------------------------------------------------------------------
+// Name: freeMem
+// Func: Wrapper for global memory manager when freeing memory.
+// Meth: Frees ptr from memory and updates global memory manager. If ptr = NULL
+//       then it frees all dynamically allocated memory.
+// Args: ptr = Base address of memory to free.
+// Vars: i   = Index of for loop.
+//------------------------------------------------------------------------------
+{
+    int i = 0; // Index of for loop
+
+    if (ptr != NULL) // Check for specific ptr
+    {
+        for (i = 0; i < gMan.size; i++) // Find ptr in memory manager
+        {
+            if (ptr == gMan.ptrs[i][0])
+            {
+                free(ptr);              // Free ptr from memory
+                gMan.ptrs[i][1] = 0;    // Set status of ptr to freed
+                i = gMan.size;          // Break loop
+            }
+        }
+    }
+    else
+    {
+        for (i = 0; i < gMan.size; i++) // Loop through all ptrs
+        {
+            if (gMan.ptrs[i][1] == 1)   // If ptr is allocated
+            {
+                free(gMan.ptrs[i][0]);  // Free ptr
+                gMan.ptrs[i][1] = 0;    // Set status of ptr to freed
+            }
+        }
+        gMan.size = 0;                  // Reset size of allocated mem to 0
+    }
+}
+
+int checkFile(int fd, struct stat fStats)
+//------------------------------------------------------------------------------
+// Name: checkFile
+// Func: Checks a file to make sure it is valid to be spooled.
+// Meth:
+// Args: fd     = File descriptor for file to be checked.
+//       fStats = Struct for getting metadata of a file.
+// Retn: valid  = Status of the file, if it can be spooled or not.
+//        0 = File is valid.
+//        1 = File is invalid.
+//       -1 = An error occured.
+//------------------------------------------------------------------------------
+{
+    int valid = 1; // Satus of validity of a file
+
+    if (fstat(fd, fStats) == -1)
+    [
+        perror("fstat");
+        return valid = -1;
+    ]
+
+    // Check if not regular
+    if (!(S_ISREG(fStats.st_mode)))
+    {
+       return valid;
+    }
+
+}
+
+int main(int argc, char** argv)
+//------------------------------------------------------------------------------
+// Name: main
+// Func: The main process of the program.
+// Meth:
+// Args: argc = The number of arguments passed into the program.
+//       argv = Array of arguments passed into the program.
+// Vars: fileStat = Struct for getting metadata of a file.
+// Retn:
+//------------------------------------------------------------------------------
+{
+    struct stat fileStat;   // ptr to stat structure of a file
+
+}
