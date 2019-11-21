9427ae400cd97bf9a1f074eca16e17504bdf032d
ajisrael <alexisraels@Alexs-MacBook-Pro-2.local>
2019-11-20 21:53:25 -0500

    make

--- School/Security/Programs/prog5/ssp.c
+++ School/Security/Programs/prog5/ssp.c
@@ -282,7 +282,7 @@ int main(int argc, char** argv)
         exit(1);
     }
 
-    if (reallocMem(ptr1, -1) == -1)
+    if (reallocMem(ptr1, 1) == -1)
     {
         freeMem(NULL);
         exit(1);
