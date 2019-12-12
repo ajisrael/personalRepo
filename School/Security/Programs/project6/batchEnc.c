35e52c9453234ebb4869002e0804c70755f3e66d
ajisrael <alexisraels@rover-214-89.rovernet.mtu.edu>
2019-12-12 16:00:16 -0500

    Secure coding revision 1

--- School/Security/Programs/project6/batchEnc.c
+++ School/Security/Programs/project6/batchEnc.c
@@ -1,6 +1,6 @@
 //
 //  Useage: (1) fge file start end  OR  
-//                (2) fge -f listFile
+//          (2) fge -f listFile
 //
 //  When invoked as fge file start end, it encrypts the contents of the file 
 //  named on the command line starting at the offset start and ending and the
@@ -18,23 +18,50 @@
 #include <stdio.h>
 #include <string.h>
 
+/// CHANGE: Added for getuid() and geteuid() function calls
+#include <unistd.h>
+
 #define NAMLEN 20
 
 struct fdata {
   char name[NAMLEN];
-  /// Changed from unsigned short to int
+  /// CHANGE: Changed from unsigned short to int
   int start;
   int end;
 };
 
+/// CHANGE: Added function to set core dump to zero.
+void secureCoreDump()
+// -----------------------------------------------------------------------------
+// Name: secureCoreDump
+// Func: Sets the size of the core dump to zero.
+// Meth: Gets the current core settings, sets the current and max limit
+//       attributes to zero and sets that as the settings for the core.
+// Vars: plimits = Rlimit structure for getting and setting core attributes.
+// -----------------------------------------------------------------------------
+{
+    struct rlimit plimits;
+    if (getrlimit(RLIMIT_CORE, &plimits) < 0)
+    {
+        perror("core_get");
+        exit(1);
+    }
+    plimits.rlim_cur = 0;
+    plimits.rlim_max = 0;
+    if (setrlimit(RLIMIT_CORE, &plimits) < 0)
+    {
+        perror("core_set");
+        exit(1);
+    }
+}
  
-
-unsigned int pastWhite(int fd){
+/// CHANGE: Changed from unsigned int to char
+char pastWhite(int fd){
      char ch;
      int ret;
 
     
-     ret=read(fd,&ch,1);
+     ret = read(fd,&ch,1);
      //-- Is line empty?
      if (ret==0)   return(0);
      if (ret<0)    return(-1);
@@ -55,7 +82,8 @@ unsigned int pastWhite(int fd){
 
 }
 
-unsigned getFileList(struct fdata *fileList[], char *file)
+/// CHANGE: Return type from unsigned to int
+int getFileList(struct fdata *fileList[], char *file)
 {
 
      char num[5];   //-- Temporary storage for text value of input offset
@@ -65,62 +93,78 @@ unsigned getFileList(struct fdata *fileList[], char *file)
      char ch;       //-- Store a character while handling 
      int ret;       //-- Return status 
      
-     nctr=0;fctr=0;
-     fd=open(file,O_RDWR|O_CREAT,644);
+     nctr = 0;
+     fctr = 0;
+  	 /// CHANGE: O_RDWR to O_RDONLY remove O_CREAT  added O_NOFOLLOW
+     fd = open(file, O_RDONLY | O_NOFOLLOW);
+  	 if (fd == -1)
+     {
+       perror("fileList_open");
+     }
 
 START:
 
     //--- Past initial white space
-    ch=pastWhite(fd);
-    if (ch==0) return(fctr); //-- Last line blank without '\n'
-    if (ch<0) return(-1);    //-- Error
-    if (ch==1) return(fctr); //-- Last line blank with '\n'
+    ch = pastWhite(fd);
+    if (ch == 0) return(fctr); //-- Last line blank without '\n'
+    if (ch < 0)  return(-1);   //-- Error
+    if (ch == 1) return(fctr); //-- Last line blank with '\n'
     
     //-- Get the file name
-    nctr=0;
-    for (read(fd,&ch,1);
-         ch!=32&&ch!=9&&ch!='\n';
-         fileList[fctr]->name[nctr++]=ch,read(fd,&ch,1));
+    nctr = 0;
+    for (read(fd, &ch,1);
+         ch != 32 && ch != 9 && ch != '\n';
+         fileList[fctr]->name[nctr++] = ch, read(fd, &ch, 1));
     
-    lseek(fd,lseek(fd,0,SEEK_CUR)-1,SEEK_SET);
+    lseek(fd, lseek(fd, 0, SEEK_CUR) - 1, SEEK_SET);
     if (ch=='\n') return(1);
     fileList[fctr]->name[nctr]=0;
 
     
     //-- Past white space
-    ch=pastWhite(fd);
-    if (ch==0)return(-2);  //-- EOF too soon
-    if (ch<0) return(-3);  //-- Error
-    if (ch==1) return(-4); //-- Line return too soon
+    ch = pastWhite(fd);
+    if (ch == 0) return(-2);  //-- EOF too soon
+    if (ch < 0)  return(-3);  //-- Error
+    if (ch == 1) return(-4);  //-- Line return too soon
 
     //-- Get the first offset
-    nctr=0;
-    for (read(fd,&ch,1);
-         ch!=32&&ch!=9&&ch!='\n';
-         num[nctr++]=ch,read(fd,&ch,1));
-    lseek(fd,lseek(fd,0,SEEK_CUR)-1,SEEK_SET);
+    nctr = 0;
+    for (read(fd, &ch,1);
+         ch!=32 && ch != 9 && ch != '\n';
+         num[nctr++] = ch, read(fd, &ch, 1));
+    lseek(fd, lseek(fd, 0, SEEK_CUR) - 1, SEEK_SET);
     
     if (ch=='\n') return(-5);   //--- Missing last offset
     num[nctr]=0;
-    fileList[fctr]->start=atoi(num);
+    fileList[fctr]->start = atoi(num);
 
 
     //-- Past white space
     ch=pastWhite(fd);
-    if (ch==0)return(-6);  //-- Line ended too soon
-    if (ch<0) return(-7);  //-- Read error
+    if (ch == 0)return(-6);  //-- Line ended too soon
+    if (ch < 0) return(-7);  //-- Read error
 
     //-- Get the second offset
-    nctr=0;
-    for (ret=read(fd,&ch,1);(ch!=32&&ch!=9&&ch!='\n'&&ret==1);num[nctr++]=ch,ret=read(fd,&ch,1));
+    nctr = 0;
+    for (ret = read(fd, &ch, 1);
+        (ch != 32 && ch != 9 && ch != '\n' && ret == 1);
+         num[nctr++]= ch, ret = read(fd, &ch, 1));
+
     num[nctr]=0; 
-    fileList[fctr]->end=atoi(num);
-    if (ch=='\n') {fctr++;goto START;} //--- Last offset followed by ;\n'
-    if (ret==0) return(++fctr);        //--- Last offset at end of file
+    fileList[fctr]->end = atoi(num);
+    if (ch == '\n') {fctr++;goto START;} //--- Last offset followed by ;\n'
+    if (ret == 0) return(++fctr);        //--- Last offset at end of file
         
     //--- Get trailing whitespace
     lseek(fd,lseek(fd,0,SEEK_CUR)-1,SEEK_SET);
-    ch=pastWhite(fd);
+    ch = pastWhite(fd);
+    
+  	if (close(fd) == -1)
+    {
+			perror("fileList_close");
+      exit(1);
+    }
+      
     if (ch<0) return(-1);
     if (read(fd,&ch,1)==0) return(fctr);
     if (ch==10) {fctr++;goto START;}
@@ -134,12 +178,26 @@ int encryptFile(char *file, int start, int end)
   int len;
  
 #ifdef DEBUG 
-  printf("Encrypting <%s> from %d to %d \n",file,start,end);  
+  printf("Encrypting <%s> from %d to %d \n", file, start, end);  
 #endif
-  plain=malloc(sizeof(char)*(end-start));
-  fd=open(file,O_RDWR);
-  read(fd,plain,end-start);
-  encryptWithPhrase(plain,file,end-start);
+  plain = malloc(sizeof(char) * (end-start));
+  fd = open(file,O_RDWR);
+  read(fd, plain, end-start);
+ if (encryptWithPhrase(plain, file, end-start) == 1)
+ {
+   return -1;
+ }
+  return 0;
+}
+
+/// CHANGE: Easier way to free file list ptrs when handling errors
+void freeFileList(struct fdata **fileList)
+{
+     for(i=0;i<10;i++)
+         {
+           free(fileList[i]);
+         }
+         free(fileList[i]);
 }
 
 int main(int argc, char *argv[]){
@@ -147,12 +205,36 @@ int main(int argc, char *argv[]){
   struct fdata **fileList;
   int entries; 
   int i;
+  
+  /// CHANGE: get uids of current process for preventing root from running
+  uid_t 					uid  = getuid();   // UID of current process
+  uid_t 					euid = geteuid();  // EUID of current process
 
-  /// Check input
+  /// CHANGE: Check input
   if (argc != 2 || argc != 3)
   {
     printf("Invocation: batchEnc file start end or -f file.")
     exit(1);
+  }
+  
+  /// CHANGE: Set core dump size to zero
+  secureCoreDump();
+  
+  /// CHANGE: Clear process environment
+  if (clearenv() != 0)
+  {
+      perror("clearenv");
+      exit(1);
+  }
+  
+  /// CHANGE: Set the umask to prevent group and world bits from being set
+  umask(077);
+  
+  /// CHANGE: Prevent process from running as root
+  if (euid == 0 || uid == 0)
+  {
+      printf("root: ssp cannot be run as root.\n");
+      exit(1);
   }
 
   //-- Create space for an array of structure pointers.
@@ -165,11 +247,51 @@ int main(int argc, char *argv[]){
   
   //-- Branch based on whether user ones one segment encrypted or has a list
   if (strcmp("-f", argv[1]) == 0){
-    entries=getFileList(fileList,argv[2]);
+    entries = getFileList(fileList, argv[2]);
+    
+    //CHANGE: Added switch statement for looking error messages of getFileList
+    switch(entries)
+    {
+      case -1:
+        perror("ch < 1");
+        freeFileList(fileList);
+        exit(1);
+        break;
+      case -2:
+        perror("EOF too soon");
+        freeFileList(fileList);
+        exit(1);
+        break;
+      case -3:
+      	perror("second ch < 1");
+        freeFileList(fileList);
+        exit(1);
+        break;
+      case -4:
+        perror("Line return too soon");
+        freeFileList(fileList);
+        exit(1);
+        break;
+      case -5:
+        perror("Missing last offset");
+        freeFileList(fileList);
+        exit(1);
+        break;
+      case -6:
+        perror("Line ended too soon");
+        freeFileList(fileList);
+        exit(1);
+        break;
+      case -7:
+        perror("Read error");
+        freeFileList(fileList);
+        exit(1);
+        break;
+    }
   } else {
-     entries=1;
-     /// Removed double malloc
-     // fileList[0] = malloc(sizeof(struct fdata));
+     entries = 1;
+     /// CHANGE: Removed double malloc
+     /// fileList[0] = malloc(sizeof(struct fdata));
      strlen(argv[1] )
      strcpy(fileList[0]->name, argv[1]);     
      fileList[0]->start = atoi(argv[2]);
@@ -177,7 +299,19 @@ int main(int argc, char *argv[]){
   }
  
   //-- The encryption
-   for (i=0;i<entries;i++)
-       encryptFile(fileList[i]->name,fileList[i]->start,fileList[i]->end);
+  /// CHANGE: Freeing file list on error
+   for (i=0; i < entries; i++)
+   {
+       if(encryptFile(fileList[i]->name, fileList[i]->start, fileList[i]->end)==-1)
+       {
+         freeFileList(fileList);
+         exit(1);
+       }
+   }
+  /// CHANGE: freeing filelist
+	freeFileList(fileList);
+  
+  /// CHANGE: exit with good code
+  exit(0);
 }
 
--- School/Security/Programs/project6/encryptWithPhrase.c
+++ School/Security/Programs/project6/encryptWithPhrase.c
@@ -7,126 +7,484 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include "encryptWithPhrase.h"
+#include <time.h>
 
 #define SHA1LENGTH  20
 #define BFKEYLEN    16
 #define BLOCKSIZE    8  /* Blowfish block size */
 #define BUFSIZE     10
-void 
-initialize_key(unsigned char *key, int length)
+
+/// CHANGE: Function added to lock memory
+int lockMemory(char * addr, size_t size) {
+// -----------------------------------------------------------------------------
+// Name: lockMemory
+// Func: Locks pages in memory at address addr for size / pageSize pages.
+// Meth: Gets the system page size, caclulates the number of pages 
+//       required, and locks them.
+// Args: addr = Location of variable to lock in memory.
+//       size = Size in bytes of variable to lock in memory.
+// Vars: page_offset = Offset from size to page.
+//       page_size   = Page size on system.
+// Retn: Return value from mlock().
+// -----------------------------------------------------------------------------
+    unsigned long page_offset;  // Offest from size to page
+    unsigned long page_size;    // Page size of system
+
+    // Set page size and offset
+    page_size = sysconf(_SC_PAGE_SIZE);
+    page_offset = (unsigned long) addr % page_size;
+
+    addr -= page_offset; // Adjust addr to pg boundary
+    size += page_offset; // Adjust size w/page_offset
+    return ( mlock(addr, size) ); // Lock the memory
+} 
+
+/// CHANGE: Funciton added to unlock memory
+int unlockMemory(char * addr, size_t size) 
+// -----------------------------------------------------------------------------
+// Name: unlockMemory
+// Func: Unlocks pages in memory at address addr for size / pageSize pages.
+// Meth: Sets the value of the contents of the char array at addr to NULL. Then
+//       gets the system page size, caclulates the number of pages required,
+//       and unlocks them.
+// Args: addr = Location of variable to unlock in memory.
+//       size = Size in bytes of variable to unlock in memory.
+// Vars: page_offset = Offset from size to page.
+//       page_size   = Page size on system.
+// Retn: Return value from munlock().
+// -----------------------------------------------------------------------------
+{
+    unsigned long page_offset;  // Offest from size to page
+    unsigned long page_size;    // Page size of system
+
+    // Clear variable to NULL
+    bzero(addr, size);
+
+    // Set page size and offset
+    page_size = sysconf(_SC_PAGE_SIZE);
+    page_offset = (unsigned long) addr % page_size;
+
+    addr -= page_offset; // Adjust addr to page boundary
+    size += page_offset; // Adjust size with page_offset
+    return ( munlock(addr, size) ); // Unlock the memory
+} 
+
+/// CHANGE: Added function to set core dump to zero.
+int secureCoreDump()
+// -----------------------------------------------------------------------------
+// Name: secureCoreDump
+// Func: Sets the size of the core dump to zero.
+// Meth: Gets the current core settings, sets the current and max limit
+//       attributes to zero and sets that as the settings for the core.
+// Vars: plimits = Rlimit structure for getting and setting core attributes.
+// 			 status  = The status of the function. 0 = good, -1 = error occured.
+// -----------------------------------------------------------------------------
+{
+  	int status = 0; // status of function
+    struct rlimit plimits;
+    if (getrlimit(RLIMIT_CORE, &plimits) < 0)
+    {
+        perror("core_get");
+    		status =-1;
+    }
+    plimits.rlim_cur = 0;
+    plimits.rlim_max = 0;
+    if (setrlimit(RLIMIT_CORE, &plimits) < 0)
+    {
+        perror("core_set");
+        status = -1;
+    }
+  	
+  	return status;
+}
+
+/// POTCHANGE: May need this if the other one is not secure
+// void initializeKey(unsigned char *key , int length)
+// {
+// 	FILE * rng;
+// 	int num=0;
+// 	rng = fopen("/dev/urandom","r");
+// 	while(num<length)
+// 	{
+// 		num+= fread(&key[num],1,length-num,rng);
+// 	}
+// 	fclose(rng);
+// }
+
+void initialize_key(unsigned char *key, int length)
 {
   int more;
   int intsize;
   int done;
   int left;
 
-  srand(10);
+  srand(time(NULL));
   
-  intsize=sizeof(int);
-  left=length;
-  done=0;
-  while (done<length)
+  intsize = sizeof(int);
+  left = length;
+  done = 0;
+  while (done < length)
     { 
-      more=rand();
+      more = rand();
       if (left >= intsize) 
-	{	  
-	  strncpy(key+done,(unsigned char*)&more,intsize);
-	  left-=intsize;done+=intsize;
-	}else{
- 	  strncpy(key+done,(unsigned char *)&more,left);
-	  done+=left;	 
-	}
+			{
+	  		strncpy(key+done, (unsigned char*)&more, intsize);
+	  		left -= intsize;
+    		done += intsize;
+			}else{
+ 	  		strncpy(key+done, (unsigned char *)&more, left);
+	  		done += left;	 
+			}
     }
 }
-unsigned char  *
-allocate_ciphertext(int mlen)
+
+unsigned char* allocate_ciphertext(int mlen)
 {
 	/* Alloc enough space for any possible padding. */
-	return (unsigned char *)malloc(mlen + BLOCKSIZE);
+	return (unsigned char *) malloc(mlen + BLOCKSIZE);
 }
-void 
-encrypt_and_print(EVP_CIPHER_CTX * ectx, char *msg, int mlen,
-                  char *res, int *olen, FILE * f)
+
+/// CHANGE: Returns int for error checking
+int encrypt_and_print(EVP_CIPHER_CTX * ectx, char *msg, int mlen,
+                       char *res, int *olen, FILE * f)
 {
   
-  int             extlen;
+  int extlen;
   
-  EVP_EncryptUpdate(ectx, res, olen, msg, mlen);
-  EVP_EncryptFinal(ectx, &res[*olen], &extlen);
+  /// CHANGE: Added status variable for detecting errors
+  int status = 0;
   
-  *olen += extlen;  
+  /// CHANGE: Error checking on EVP functions
+  if (EVP_EncryptUpdate(ectx, res, olen, msg, mlen) == 0)
+  {
+  	perror("encrypt_update");
+    status = -1;
+  }
+  
+  if (status == 0 && EVP_EncryptFinal(ectx, &res[*olen], &extlen) == 0)
+  {
+  	perror("encrypt_update");
+    status = -1;
+  }
+  
+  *olen += extlen; 
+  return status;
 }
 
 int encryptWithPhrase(char *plaintext, char *file, int size)
 {
-        unsigned char   fkey[BFKEYLEN];       /* File encryption key.  More 
+  //default size of passphrase
+  int psize = 80;
+  unsigned char   fkey[BFKEYLEN];       /* File encryption key.  More 
                                                  bytes than needed.  */
-        unsigned char   kkey[SHA1LENGTH];     /* Raw key key bytes */
+  unsigned char   kkey[SHA1LENGTH];     /* Raw key key bytes */
 
-        EVP_MD_CTX      hctx;     	      /* Context for hash */
-        int             hlen;	              /* Length of hash */
+  EVP_MD_CTX      hctx;     	          /* Context for hash */
+  int             hlen;	                /* Length of hash */
 
-        EVP_CIPHER_CTX  ctx;                  /* Context for encryption */
+  EVP_CIPHER_CTX  ctx;                  /* Context for encryption */
 	EVP_CIPHER      *cipher;              /* Cipher */
 	char            ivec[EVP_MAX_IV_LENGTH] = {0}; 
-	int             ret;	              /* Return status */
-        int             fdk;                  /* Key to this file */
-        int             fdp;                  /* Plaintext from this file */
-        int             fde;
-        char           *encFile;
-        char           *keyFile;
+	int             ret;	                /* Return status */
+  int             fdk;                  /* Key to this file */
+  int             fdp;                  /* Plaintext from this file */
+  int             fde;
+  char           *encFile;
+  char           *keyFile;
 	unsigned char  *ciphertext;           /* Pointer to ciphertext */
 	int             ctlen;                /* Length of ciphertext */
-        char            phrase[80];
+  /// CHANGE: malloc instead of static size (starts at 80)
+  char            phrase = malloc(sizeof(char) * size);
 	int             namelen;
-	
- 	
+  
+  /// CHANGE: get uids of current process for preventing root from running
+  uid_t 					uid  = getuid();   // UID of current process
+  uid_t 					euid = geteuid();  // EUID of current process
+  
+  // CHANGE: Clear process environment
+  if (clearenv() != 0)
+  {
+      perror("clearenv");
+      exit(1);
+  }
+  
+  /// CHANGE: Secure the core dump size to zero
+  // Set core dump size to zero
+  if(secureCoreDump() == -1)
+  {
+    perror("secure_core_dump");
+    free(phrase);
+    exit(1);
+  }
+  
+  /// CHANGE: Set the umask to prevent group and world bits from being set
+  umask(077);
+  
+  /// CHANGE: Prevent process from running as root
+  if (euid == 0 || uid == 0)
+  {
+      printf("root: ssp cannot be run as root.\n");
+      exit(1);
+  }
+  
 	/* Generate the key -- may be more bytes than needed*/
-        initialize_key(fkey,BFKEYLEN);
+  initialize_key(fkey, BFKEYLEN);
 
-        /* Get the passphrase. Will run digest over this to produce key */ 
-        printf("Input passphrase: ");
-        gets(phrase);
+	if (lockMemory(fkey,sizeof(fkey))) == -1) 
+  {
+    perror("fkey_lockMemory");
+    free(phrase);
+    exit(1);
+  }
+  
+  /* Get the passphrase. Will run digest over this to produce key */ 
+  printf("Input passphrase: ");
+  
+  /// CHANGE: reads forever for an really long password
+  int read = 1;
+  while (read == 1 || phrase[read - 2] != '\n')
+  {
+     if (read > psize)
+        in = realloc(phrase, sizeof(char) * ++psize);
+     scanf("%c", &phrase[read - 1]);
+     read++;
+  }
+  phrase[read - 2] = '\0';
+  
+  if (lockMemory(phrase, sizeof(char) * strlen(phrase)) == -1) 
+  {
+    perror("phrase_lockMemory");
+    free(phrase);
+    exit(1);
+  }
+  
+
+  /* Compute the digest. Output is the key key.*/
+
+  /// CHANGE: Check all return codes of EVP calls  
+  if(EVP_DigestInit(&hctx, EVP_sha1())==0)
+  {
+    perror("digest_init");
+    free(phrase);
+    exit(1);
+  }
+  
+  /// CHANGE: Lock kkey into memory
+  if (lockMemory(kkey,sizeof(kkey))) == -1) 
+  {
+    perror("kkey_lockMemory");
+    free(phrase);
+    exit(1);
+  }
+  
+  /// CHANGE: Check all return codes of EVP calls  
+  if(EVP_DigestUpdate(&hctx, phrase, strlen(phrase))==0)
+  {
+  	perror("digest_update");
+    free(phrase);
+    free(hctx);
+    exit(1);
+  } 
+  /* Won't include the string terminator*/
+  
+  if(EVP_DigestFinal(&hctx, kkey, &hlen)==0)
+  {
+    perror("digest_final");
+    free(phrase);
+    free(hctx);
+    exit(1);
+  }
+  
+  /// CHANGE: Does not clean up or free hctx
+  if(EVP_MD_CTX_cleanup(hctx)==0)
+  {
+    perror("EVP_MD_CTX_cleanup");
+    free(phrase);
+    free(hctx);
+    exit(1);
+  }
+  free(hctx);
+  
+  /// CHANGE: Setting phrase to zero in memory and unlock the memory
+  if(unlockMemory(phrase,sizeof(char)*strlen(phrase))==-1)
+  {
+    	perror("phrase_unlockMemory");
+    	free(phrase);
+    	exit(1);
+  }
+  
+  /// CHANGE: Freeing phrase
+  free(phrase);
+  
+  /* Encrypt key */
+  cipher = (EVP_CIPHER *) EVP_bf_cbc();              /* Blowfish CBC mode */
+  
+  /// CHANGE: Error checking of EVP funtion
+  if(EVP_EncryptInit(&ctx, cipher, kkey, ivec) == 0) /* Kkey to encrypt kfile */
+  {
+      perror("kFile_encrypt_init");
+      exit(1);
+  } 
+  
+  /// CHANGE: Error checking for unlock memory, and unlocking memory
+  if (unlockMemory(kkey,sizeof(kkey))) == -1) 
+  {
+    perror("kkey_unlockMemory");
+    exit(1);
+  }
+  
+  ciphertext = allocate_ciphertext(BFKEYLEN);
+  /// CHANGE: check ciphertext gets malloc'd
+  if(ciphertext == NULL)
+  {
+     perror("allocate_ciphertext");
+     EVP_MD_CTX_cleanup(ctx);
+     free(ctx);
+     exit(1);
+  }
 	
-        /* Compute the digest. Output is the key key.*/
-        EVP_DigestInit(&hctx, EVP_sha1());
-        EVP_DigestUpdate(&hctx, phrase, strlen(phrase)); /* Won't include the
-							 string terminator*/
-        EVP_DigestFinal(&hctx, kkey, &hlen);	
+  /// CHANGE: check encrypt_and_print return codes
+  if (encrypt_and_print(&ctx, fkey, BFKEYLEN, /* Encrypt fkey output is */
+                     ciphertext, &ctlen, stdout) == -1)   /* ciphertext */
+  {
+		 EVP_MD_CTX_cleanup(ctx);
+     free(ctx);
+     exit(1);
+  }
+  
+  //CHANGE: Moved ctx cleanup higher up
+  if(EVP_CIPHER_CTX_cleanup(&ctx)==0)
+  {
+    perror("cleanup_ctx");
+    free(ctx);
+    exit(1);
+  } 
+
+  /* Write encrypted key to file */
+  namelen = strlen(file) + strlen(".key") + 1;	
+  keyFile = malloc(namelen);
+  
+  /// CHANGE: Error check for malloc call
+  if (keyFile == NULL)
+  {
+		perror("keyFile_malloc");
+    exit(1);
+  }
+  
+	strcpy(keyFile, file);
+	strcat(keyFile, ".key");
+ 	keyFile[namelen-1] = 0;
 	
-       /* Encrypt key */
-       cipher = (EVP_CIPHER *)EVP_bf_cbc();       /* Blowfish CBC mode */
-       EVP_EncryptInit(&ctx, cipher, kkey, ivec); /* Kkey to encrypt kfile */
-       ciphertext = allocate_ciphertext(BFKEYLEN);
-       encrypt_and_print(&ctx, fkey, BFKEYLEN, /* Encrypt fkey output is */
-                          ciphertext, &ctlen, stdout);     /* ciphertext */
+  fdk = open(keyFile,O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR );
+  /// CHANGE: Error checking of open
+  if (fdk == -1)
+  {
+    perror("keyFile_open");
+    free(keyFile);
+    exit(1);
+  }
+  
+  /// CHANGE: Free keyFile
+  free(keyFile);
+  
+  /// CHANGE: Error checking of write
+  if (write(fdk, ciphertext, ctlen) == -1)
+  {
+    perror("keyFile_write");
+    close(fdk);
+    exit(1);
+  }
+  
+  /// CHANGE: Error checking of close
+	if (close(fdk) == -1)
+  {
+    perror("keyFile_close");
+    exit(1);
+  }
+
+  namelen = strlen(file) + strlen(".key") + 1;	
+  encFile = malloc(namelen);
+  
+  /// CHANGE: Error checking of malloc
+  if (encFile == NULL)
+  {
+    perror("encFile_malloc");
+    exit(1);
+  }
+  
+	strcpy(encFile, file);
+	strcat(encFile, ".enc");
+	encFile[namelen - 1] = 0;
 	
-         /* Write encrypted key to file */
-         namelen=strlen(file)+strlen(".key")+1;	
-         keyFile=malloc(namelen);
-	 strcpy(keyFile,file);
-	 strcat(keyFile,".key");
- 	 keyFile[namelen-1]=0;
-	 
-        fdk=open(keyFile,O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR );
-        write(fdk,ciphertext,ctlen); 
-	close(fdk);
-
-        namelen=strlen(file)+strlen(".key")+1;	
-        encFile=malloc(namelen);
-	strcpy(encFile,file);
-	strcat(encFile,".enc");
-	encFile[namelen-1]=0;
+  fde = open(encFile, O_CREAT | O_TRUNC | O_WRONLY | O_NOFOLLOW | O_APPEND, S_IRUSR | S_IWUSR);
 	
-        fde=open(encFile,O_CREAT | O_TRUNC | O_WRONLY | O_NOFOLLOW | O_APPEND, S_IRUSR | S_IWUSR);
+  /// CHANGE: Error checking of open
+  if (fde == -1)
+  {
+    perror("encFile_open");
+    free(encFile);
+    exit(1);
+  }
+  
+  /// CHANGE: Free encFile
+  free(encFile);
 
-        ret=EVP_CIPHER_CTX_cleanup(&ctx);
+  cipher = (EVP_CIPHER *) EVP_bf_cbc();
+  
+  /// CHANGE: Error checking of EVP funtion
+  if (EVP_EncryptInit(&ctx, cipher, fkey, ivec) == 0) /* fkey to encrypt file */
+  {
+    perror("encFile_encrypt_init")
+    close(fde);
+    exit(1);
+  }
+  
+  ciphertext = allocate_ciphertext(size+1);
+  
+	/// CHANGE: Check for proper allocation of ciphertext
+  if (ciphertext == NULL)
+  {
+    perror("encFile_allocate_ciphertext");
+    close(fde);
+    exit(1);
+  }
+  encrypt_and_print(&ctx, plaintext, size, 
+  ciphertext, &ctlen, stdout);        /* ciphertext */
+  
+  //CHANGE: unlocking fkey in memory
+ 	if (unlockMemory(fkey,sizeof(fkey))) == -1) 
+  {
+    perror("unlock Memory Error fkey");
+    close(fde);
+    exit(1);
+  }
 
-        cipher=(EVP_CIPHER *)EVP_bf_cbc();
-        EVP_EncryptInit(&ctx, cipher, fkey, ivec); /* fkey to encrypt file */
-        ciphertext = allocate_ciphertext(size+1);
-        encrypt_and_print(&ctx, plaintext, size, 
-                          ciphertext, &ctlen, stdout);        /* ciphertext */
+  //CHANGE cleanup ctx again
+  if(EVP_CIPHER_CTX_cleanup(&ctx)==0)
+  {
+    perror("cleanup_ctx");
+    free(ctx);
+    close(fde);
+    exit(1);
+  } 
+  
+  free(ctx); 
+  
+  /// CHANGE: Error checking write
+  if(write(fde,ciphertext,ctlen) == -1)
+  {
+    perror("writing ciphertext");
+    close(fde);
+    exit(1);
+  }
+  
+  /// CHANGE: Error chekcing Close
+  if(close(fde) == -1)
+  {
+    perror("closing fde");
+    exit(1);
+  }
 
-        write(fde,ciphertext,ctlen);
+  /// CHANGE: Exit status okay
+  exit(0);
 }
