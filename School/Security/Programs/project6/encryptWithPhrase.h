fcc9f94aed0de86db3ae8ee68967b4663e94219b
ajisrael <alexisraels@rover-214-89.rovernet.mtu.edu>
2019-12-12 16:28:30 -0500

    header fix

--- School/Security/Programs/project6/encryptWithPhrase.h
+++ School/Security/Programs/project6/encryptWithPhrase.h
@@ -2,6 +2,7 @@
 void initialize_key(unsigned char *key, int length);
 void fprint_string_as_hex(FILE * f, unsigned char *s, int len);
 unsigned char  *allocate_ciphertext(int mlen);
-void encrypt_and_print(EVP_CIPHER_CTX * ectx, char *msg, int mlen,
-                  char *res, int *olen, FILE * f);
+/// CHANGE: params take in unsigned char * and returns int for error checking
+int encrypt_and_print(EVP_CIPHER_CTX * ectx, unsigned char *msg, int mlen,
+                  unsigned char *res, int *olen, FILE * f);
 int encryptWithPhrase(char *plaintext, char *file, int size);
