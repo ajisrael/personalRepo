9feefcdc08574d8d4458fc6fdd2ae92854e76a3b
ajisrael <ajisrael@mtu.edu>
2020-06-09 09:10:00 -0400

    check in

--- TitanTag/TriggerTesting/TriggerTesting.ino
+++ TitanTag/TriggerTesting/TriggerTesting.ino
@@ -175,7 +175,7 @@ void setup()
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // GOAL IS TO KEEP THIS EMPTY
 }
 
 // ================================================================================EOF
