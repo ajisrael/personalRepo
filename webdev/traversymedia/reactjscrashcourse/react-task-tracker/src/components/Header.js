0b87464d8e834d2e12e38cbd86faba182794bce2
ajisrael <ajisrael@mtu.edu>
2021-01-23 15:49:53 -0500

    nodejs cc

--- webdev/traversymedia/reactjscrashcourse/react-task-tracker/src/components/Header.js
+++ webdev/traversymedia/reactjscrashcourse/react-task-tracker/src/components/Header.js
@@ -1,26 +1,36 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
-        </header>
-    )
+  const location = useLocation()
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker',
+  title: 'Task Tracker',
 }
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 }
 
-// CSS inject js
-// const headingSytle = {
-//     color: 'red', backgroundColor: 'black'
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
 // }
 
 export default Header
